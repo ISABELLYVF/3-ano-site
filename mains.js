0f8a951cad1a8381d0db4995ada0f2b11e658cbb
ISABELLYVF <126249501+ISABELLYVF@users.noreply.github.com>
2024-04-02 08:01:33 -0300

    Update mains.js

--- mains.js
+++ mains.js
@@ -1,11 +1,11 @@
 const botoes = document.querySelectorAll(".botao");
-console.log(botaes);
-for(left i=0;i <botoes.length;i++){
+
+for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
-
-        for(left J=0;)
-
-        botoes[i].classList.add("ativo")
+        
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
     }
-    console.log()
 }
